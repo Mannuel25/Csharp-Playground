a57977d5d3f6a7e7b7456287a3f0c039fd82793a
mannuel25 <oluwasegunprosperity@gmail.com>
2025-07-17 21:23:30 +0100

    'variables..'

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to C# Class!");
+
+/* A multi-
+ * line comment */
+
+//string name, message;
+Console.WriteLine("What's your name?:");
+string name = Console.ReadLine();
+string message = "Hello " + name;
+Console.WriteLine(message);
+
+string number = "3";
+Console.WriteLine(Convert.ToInt32(number));
+int converted_number = int.Parse(number);
+Console.WriteLine("The converted number is : " +  converted_number);
